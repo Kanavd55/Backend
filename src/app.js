5f458d077fe3e7bd76c3ac04592461fad324dbfb
Kanav Dahat <kanavdahat55@gmail.com>
2025-03-10 15:31:17 +0530

    Learning Routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,30 @@ const express = require('express');
 
 const app =express();
 
-app.use("/",(req,res)=>{
-    res.send("Hell")
+app.get("/user",(req,res)=>{
+
+    console.log(req.query)
+    res.send({
+        firstname:"kanav",
+        lastname:"Dahat"
+    })
 })
 
-app.use("/test",(req,res)=>{
-    res.send("Hello World")
+app.get("/user/:userId",(req,res)=>{
+
+    console.log(req.params)
+    res.send({
+        firstname:"kanav",
+    })
+})
+
+app.post("/hello",async (req,res)=>{
+    res.send("Data saved successfully to the db ")
+})
+
+
+app.get("/",(req,res)=>{
+    res.send("Hell")
 })
 
 app.listen(3000,()=>{
