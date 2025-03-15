abe7d6c6b3a4ad066641ae29a80386140c6e67de
Kanav Dahat <kanavdahat55@gmail.com>
2025-03-15 14:08:47 +0530

    Added schema level validation and api level validation

--- src/app.js
+++ src/app.js
@@ -4,17 +4,61 @@ const connectDB = require("./config/database")
 const User = require('./models/user')
 const app =express();
 
-app.get("/user",(req,res,next)=>{
+app.use(express.json());  //To convert json object into js object
+app.get("/user",async (req,res,next)=>{
+    const userEmail = req.body.emailId;
+    try{
+        const user =await User.find({emailId:userEmail});
+        if(user.length === 0){
+            res.status(200).send("Something went wrong")
+        }else{
+            res.send(user)
+        }
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
 
-    console.log(req.query)
-    next()
+app.get("/feed",async (req,res,next)=>{
+    try{
+        const users =await User.find({});
+        res.send(users)
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+app.delete("/user",async (req,res)=>{
+    try{
+        const userId = req.body.userId;
+        const user = await User.findByIdAndDelete(userId)
+        res.send("user deleted successfully")
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+app.patch("/user",async (req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    const ALLOWED_UPDATES = ["userid","photurl","about","gender","age","skills"]
+    const isUpdateAllowed = Object.keys(data).every((k)=>ALLOWED_UPDATES.includes(k.toLowerCase()));
+    if(!isUpdateAllowed){
+        throw new Error("Update not allowed")
+    }
+    if(data?.skills?.length>10){
+        throw new Error("Skills cannot be more than 10")
+    }
+    try{
+        const user = await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after",runValidators:true})
+        res.send("User updated successfully")
+    }catch(err){
+        res.status(400).send("something went wrong")
+    }
 })
 
 app.get("/user",adminAuth,(req,res)=>{
-    res.send({
-        firstname:"kanav",
-        lastname:"Dahat"
-    })
+    
 })
 
 app.get("/user/:userId",(req,res)=>{
@@ -34,6 +78,16 @@ app.post("/user",async (req,res)=>{
     })
     await user.save();
     res.send("User Added successfully ")
+})
+
+app.post("/signup",async (req,res)=>{
+    const user = new User(req.body);
+    try{
+        await user.save()
+        res.send("user added successfully")
+    }catch(err){
+        res.status(400).send("Error saving data",err.message)
+    }
 })
 
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,69 @@
 const mongoose = require("mongoose");
+const validator = require("validator")
 
 const userSchema = new mongoose.Schema({
     firstName:{
-        type: String
+        type: String,
+        required:true,
+        minLength:4,
+        maxLength:50
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address"+email)
+            }
+        }
     },
     password:{
-        type:String
+        type:String,
+        required:true
     },
     age:{
-        type:Number
+        type:Number,
+        min:18
     },
     gender:{
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(this.valueOf.toLowerCase())){
+                throw new Error("Gender is not valid")
+            }
+        }
+    },
+    isPremium:{
+        type:Boolean,
+        default:false
+    },
+    membershipType:{
         type:String
+    },
+    photoUrl:{
+        type:String,
+        default:"https://www.google.com/imgres?q=profile%20pic&imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2015%2F10%2F05%2F22%2F37%2Fblank-profile-picture-973460_1280.png&imgrefurl=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&docid=wg0CyFWNfK7o5M&tbnid=ycNOFIKv7gjqcM&vet=12ahUKEwjgkteUgoqMAxV_cPUHHadZMhAQM3oECHsQAA..i&w=1280&h=1280&hcb=2&ved=2ahUKEwjgkteUgoqMAxV_cPUHHadZMhAQM3oECHsQAA",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid Url")
+            }
+        }
+    },
+    about:{
+        type:String,
+        default:"This is default about the user"
+    },
+    skills:{
+        type:[String]
     }
+},{
+    timestamps:true
 })
 
 //const User = mongoose.model("User",userSchema);
